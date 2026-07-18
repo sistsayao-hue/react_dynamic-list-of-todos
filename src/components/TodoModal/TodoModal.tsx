27290970e83a1219171e75a2eba364e1e39cee1a
Rodrigo <sistsayao@gmail.com>
2026-07-18 10:11:26 -0300

    Change import path for types in TodoModal

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Todo, User } from '../../types';
+import { Todo, User } from '../../types.ts';
 import { getUser } from '../../api';
 import { Loader } from '../Loader';
 interface TodoModalProps {
