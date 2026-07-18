780ebcc7b1f2f63c66599467c725ae6f1ebeec3a
Rodrigo <sistsayao@gmail.com>
2026-07-18 10:26:50 -0300

    Update TodoList.tsx

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,12 +1,12 @@
+// src/components/TodoList/TodoList.tsx
 import React from 'react';
 import { Todo } from '../../types.ts';
-
+import classNames from 'classnames';
 interface TodoListProps {
   todos: Todo[];
   selectedTodo: Todo | null;
   onTodoClick: (todo: Todo) => void;
 }
-
 export const TodoList: React.FC<TodoListProps> = ({
   todos,
   selectedTodo,
@@ -17,27 +17,21 @@ export const TodoList: React.FC<TodoListProps> = ({
       <thead>
         <tr>
           <th>#</th>
-
           <th>
             <span className="icon">
               <i className="fas fa-check" />
             </span>
           </th>
-
           <th>Title</th>
-
           <th />
         </tr>
       </thead>
-
       <tbody>
         {todos.map(todo => {
           const isSelected = selectedTodo?.id === todo.id;
-
           return (
             <tr key={todo.id} data-cy="todo">
               <td className="is-vcentered">{todo.id}</td>
-
               <td className="is-vcentered">
                 {todo.completed && (
                   <span className="icon" data-cy="iconCompleted">
@@ -45,17 +39,16 @@ export const TodoList: React.FC<TodoListProps> = ({
                   </span>
                 )}
               </td>
-
               <td className="is-vcentered is-expanded">
                 <p
-                  className={
-                    todo.completed ? 'has-text-success' : 'has-text-danger'
-                  }
+                  className={classNames({
+                    'has-text-success': todo.completed,
+                    'has-text-danger': !todo.completed,
+                  })}
                 >
                   {todo.title}
                 </p>
               </td>
-
               <td className="has-text-right is-vcentered">
                 <button
                   type="button"
@@ -65,12 +58,12 @@ export const TodoList: React.FC<TodoListProps> = ({
                 >
                   <span className="icon">
                     <i
-                      className={`far ${
-                        isSelected ? 'fa-eye-slash' : 'fa-eye'
-                      }`}
+                      className={classNames('far', {
+                        'fa-eye-slash': isSelected,
+                        'fa-eye': !isSelected,
+                      })}
                     />
                   </span>
-
                   <span>{isSelected ? 'Hide' : 'Show'}</span>
                 </button>
               </td>
@@ -81,3 +74,4 @@ export const TodoList: React.FC<TodoListProps> = ({
     </table>
   );
 };
+
